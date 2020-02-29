ed28137267cf675ce763846cf9aae7b6e94e752b
Craig Fowler <craig@csf-dev.com>
2020-02-29 16:21:52 +0000

    Resolve #8 - Change API to use less generics

--- CSF.DecoratorBuilder/ICreatesDecorator.cs
+++ CSF.DecoratorBuilder/ICreatesDecorator.cs
@@ -4,59 +4,17 @@ namespace CSF.DecoratorBuilder
 {
     public interface ICreatesDecorator<in TService> where TService : class
     {
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl>()
-            where TInitialImpl : class, TService;
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl, TParam1>(TParam1 param1)
-            where TInitialImpl : class, TService;
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl, TParam1, TParam2>(TParam1 param1, TParam2 param2)
-            where TInitialImpl : class, TService;
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3>(TParam1 param1, TParam2 param2, TParam3 param3)
-            where TInitialImpl : class, TService;
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4)
-            where TInitialImpl : class, TService;
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4, TParam5>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5)
-            where TInitialImpl : class, TService;
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6)
-            where TInitialImpl : class, TService;
-        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7)
+        ICustomizesDecorator<TService> UsingInitialImpl<TInitialImpl>(params TypedParam[] parameters)
             where TInitialImpl : class, TService;
 
-        ICustomizesDecorator<TService> UsingInitialImplType(Type initialImplType);
-        ICustomizesDecorator<TService> UsingInitialImplType<TParam1>(Type initialImplType, TParam1 param1);
-        ICustomizesDecorator<TService> UsingInitialImplType<TParam1, TParam2>(Type initialImplType, TParam1 param1, TParam2 param2);
-        ICustomizesDecorator<TService> UsingInitialImplType<TParam1, TParam2, TParam3>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3);
-        ICustomizesDecorator<TService> UsingInitialImplType<TParam1, TParam2, TParam3, TParam4>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4);
-        ICustomizesDecorator<TService> UsingInitialImplType<TParam1, TParam2, TParam3, TParam4, TParam5>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5);
-        ICustomizesDecorator<TService> UsingInitialImplType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6);
-        ICustomizesDecorator<TService> UsingInitialImplType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7);
+        ICustomizesDecorator<TService> UsingInitialImplType(Type initialImplType, params TypedParam[] parameters);
     }
 
     public interface ICreatesDecorator
     {
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl>()
-            where TInitialImpl : class;
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl, TParam1>(TParam1 param1)
-            where TInitialImpl : class;
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl, TParam1, TParam2>(TParam1 param1, TParam2 param2)
-            where TInitialImpl : class;
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3>(TParam1 param1, TParam2 param2, TParam3 param3)
-            where TInitialImpl : class;
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4)
-            where TInitialImpl : class;
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4, TParam5>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5)
-            where TInitialImpl : class;
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6)
-            where TInitialImpl : class;
-        ICustomizesDecorator UsingInitialImpl<TInitialImpl, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7)
+        ICustomizesDecorator UsingInitialImpl<TInitialImpl>(params TypedParam[] parameters)
             where TInitialImpl : class;
 
-        ICustomizesDecorator UsingInitialImplType(Type initialImplType);
-        ICustomizesDecorator UsingInitialImplType<TParam1>(Type initialImplType, TParam1 param1);
-        ICustomizesDecorator UsingInitialImplType<TParam1, TParam2>(Type initialImplType, TParam1 param1, TParam2 param2);
-        ICustomizesDecorator UsingInitialImplType<TParam1, TParam2, TParam3>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3);
-        ICustomizesDecorator UsingInitialImplType<TParam1, TParam2, TParam3, TParam4>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4);
-        ICustomizesDecorator UsingInitialImplType<TParam1, TParam2, TParam3, TParam4, TParam5>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5);
-        ICustomizesDecorator UsingInitialImplType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6);
-        ICustomizesDecorator UsingInitialImplType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7);
+        ICustomizesDecorator UsingInitialImplType(Type initialImplType, params TypedParam[] parameters);
     }
 }
--- CSF.DecoratorBuilder/ICustomizesDecorator.cs
+++ CSF.DecoratorBuilder/ICustomizesDecorator.cs
@@ -4,59 +4,17 @@ namespace CSF.DecoratorBuilder
 {
     public interface ICustomizesDecorator<in TService> where TService : class
     {
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator>()
-            where TDecorator : class, TService;
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator, TParam1>(TParam1 param1)
-            where TDecorator : class, TService;
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator, TParam1, TParam2>(TParam1 param1, TParam2 param2)
-            where TDecorator : class, TService;
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator, TParam1, TParam2, TParam3>(TParam1 param1, TParam2 param2, TParam3 param3)
-            where TDecorator : class, TService;
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4)
-            where TDecorator : class, TService;
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4, TParam5>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5)
-            where TDecorator : class, TService;
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6)
-            where TDecorator : class, TService;
-        ICustomizesDecorator<TService> ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7)
+        ICustomizesDecorator<TService> ThenWrapWith<TDecorator>(params TypedParam[] parameters)
             where TDecorator : class, TService;
 
-        ICustomizesDecorator<TService> ThenWrapWithType(Type decoratorType);
-        ICustomizesDecorator<TService> ThenWrapWithType<TParam1>(Type initialImplType, TParam1 param1);
-        ICustomizesDecorator<TService> ThenWrapWithType<TParam1, TParam2>(Type initialImplType, TParam1 param1, TParam2 param2);
-        ICustomizesDecorator<TService> ThenWrapWithType<TParam1, TParam2, TParam3>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3);
-        ICustomizesDecorator<TService> ThenWrapWithType<TParam1, TParam2, TParam3, TParam4>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4);
-        ICustomizesDecorator<TService> ThenWrapWithType<TParam1, TParam2, TParam3, TParam4, TParam5>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5);
-        ICustomizesDecorator<TService> ThenWrapWithType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6);
-        ICustomizesDecorator<TService> ThenWrapWithType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7);
+        ICustomizesDecorator<TService> ThenWrapWithType(Type decoratorType, params TypedParam[] parameters);
     }
 
     public interface ICustomizesDecorator
     {
-        ICustomizesDecorator ThenWrapWith<TDecorator>()
-            where TDecorator : class;
-        ICustomizesDecorator ThenWrapWith<TDecorator, TParam1>(TParam1 param1)
-            where TDecorator : class;
-        ICustomizesDecorator ThenWrapWith<TDecorator, TParam1, TParam2>(TParam1 param1, TParam2 param2)
-            where TDecorator : class;
-        ICustomizesDecorator ThenWrapWith<TDecorator, TParam1, TParam2, TParam3>(TParam1 param1, TParam2 param2, TParam3 param3)
-            where TDecorator : class;
-        ICustomizesDecorator ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4)
-            where TDecorator : class;
-        ICustomizesDecorator ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4, TParam5>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5)
-            where TDecorator : class;
-        ICustomizesDecorator ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6)
-            where TDecorator : class;
-        ICustomizesDecorator ThenWrapWith<TDecorator, TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7)
+        ICustomizesDecorator ThenWrapWith<TDecorator>(params TypedParam[] parameters)
             where TDecorator : class;
 
-        ICustomizesDecorator ThenWrapWithType(Type decoratorType);
-        ICustomizesDecorator ThenWrapWithType<TParam1>(Type initialImplType, TParam1 param1);
-        ICustomizesDecorator ThenWrapWithType<TParam1, TParam2>(Type initialImplType, TParam1 param1, TParam2 param2);
-        ICustomizesDecorator ThenWrapWithType<TParam1, TParam2, TParam3>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3);
-        ICustomizesDecorator ThenWrapWithType<TParam1, TParam2, TParam3, TParam4>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4);
-        ICustomizesDecorator ThenWrapWithType<TParam1, TParam2, TParam3, TParam4, TParam5>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5);
-        ICustomizesDecorator ThenWrapWithType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6);
-        ICustomizesDecorator ThenWrapWithType<TParam1, TParam2, TParam3, TParam4, TParam5, TParam6, TParam7>(Type initialImplType, TParam1 param1, TParam2 param2, TParam3 param3, TParam4 param4, TParam5 param5, TParam6 param6, TParam7 param7);
+        ICustomizesDecorator ThenWrapWithType(Type decoratorType, params TypedParam[] parameters);
     }
 }
