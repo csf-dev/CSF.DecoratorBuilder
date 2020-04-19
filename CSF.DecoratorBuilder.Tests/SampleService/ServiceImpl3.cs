b02c4f51fb608e391ba486fabc67dfe8410826eb
Craig Fowler <craig@csf-dev.com>
2020-04-19 16:32:38 +0100

    Resolve #14 - Add test coverage

---
+++ CSF.DecoratorBuilder.Tests/SampleService/ServiceImpl3.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace CSF.DecoratorBuilder.Tests.SampleService
+{
+    public class ServiceImpl3 : IServiceInterface
+    {
+        public string ServiceMethod() => GetType().Name;
+    }
+}
